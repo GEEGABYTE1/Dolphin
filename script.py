bc734f7d786aaa9b90928a7e46115f0ec5ff85c1
Jaival Patel <pateljaival05@gmail.com>
2022-03-14 19:45:53 -0400

    replication fix

--- script.py
+++ script.py
@@ -29,36 +29,42 @@ class FIFO:
             if value in current_cache_vals:
                 print(colored('HIT', 'cyan'))
                 print('-'*24)
-            else:
+                continue
+            elif len(current_cache_vals) != 0:
                 print(colored('MISS', 'red'))
                 print('-'*24)
             
             if index >= 4:
                 index = 0
-                self.cache_size = 0
+                
+                
 
             if self.cache.size >= 4:
                 if self.cache.size == 4:
                     new_node = Node(value)
-                    new_node.set_link(self.cache.head_node)
+                    prev_node = self.cache.head_node.get_link()
                     self.cache.head_node = new_node
+                    new_node.set_link(prev_node)
                 else:
                     current_node = self.cache.head_node
-                    counter = 0 
-                    while counter != index:
-                        if current_node.get_value() == value:
+                    counter = 0
+                    
+                    while current_node.get_value() != None:
+                        if counter == index:
                             current_node.value = value 
                             break
                         else:
                             current_node = current_node.get_link()
                             counter += 1
-                    self.cache.size += 1
-                    current_cache_vals[index] = value   
+                            
+                self.cache.size += 1
+                       
             else:          
                 current_cache_vals.append(value)
                 
+                self.cache.enqueue(value)
             
-            self.cache.enqueue(value)
+            current_cache_vals[index] = value
             index += 1
 
         return self.history_queues, self.history_vals
