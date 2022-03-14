6886b90e558d14ca984640114e90c8285a627754
Jaival Patel <pateljaival05@gmail.com>
2022-03-13 20:38:37 -0400

    fetching values

--- script.py
+++ script.py
@@ -2,3 +2,13 @@ from stack import Stack
 
 
 cache = Stack()
+
+class FIFO:
+    def __init__(self):
+        data_vals = str(input(': '))
+        data_vals = data_vals.split(',')
+        self.data = []
+        for character in data_vals:
+            self.data.append(int(character))
+
+        
